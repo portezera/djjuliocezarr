7eb5e1ae7a1bfe7bff5680ab2d3140654ec8d883
portezera <134521956+portezera@users.noreply.github.com>
2023-10-25 09:08:19 -0300

    a

--- main.js
+++ main.js
@@ -3,4 +3,4 @@ function tocaSomPom(){
     document.querySelector('#Som_tecla_pom')
 }
 
-document.querySelector('.Som_tecla_pom').onclick = tocaSomPom
+document.querySelector('.Som_tecla_pom').onclick = tocaSomPom;
