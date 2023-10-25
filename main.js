1f876e7a58041ae1cfd323b320a6ebf74229ad41
portezera <134521956+portezera@users.noreply.github.com>
2023-10-25 11:32:19 -0300

    a

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 alert("bolsozaro")
 function tocaSomPom(){
-    document.querySelector('#Som_tecla_pom')
+    document.querySelector('#Som_tecla_pom').play();
 }
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
 
-document.querySelector('.Som_tecla_pom').onclick = tocaSomPom;
+function tocaSomClap(){
+    document.querySelector('#Som_tecla_Clap').play();
+}
+document.querySelector('.tecla_Clap').onclick = tocaSomClap;
